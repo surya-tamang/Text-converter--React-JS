080bb02f0d2a84e0bf364983c3d2abb8ea5a484f
evil-89789 <tngsurya055@gmail.com>
2024-03-31 18:27:56 +0545

    second proprs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,33 @@
 import React from "react";
+import propTypes from "react";
 
-export default function Header(props){
-return (
-    <div>
-        <header>
-        <div className="logo">{props.title}</div>
-        <nav>
-          <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li className="dropdown"><a href="">Category</a>
-              <ul>
-                <li><a href="">Men</a></li>
-                <li><a href="">Women</a></li>
-                <li><a href="">Kid</a></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
-)
+export default function Header(props) {
+    return (
+        <div>
+            <header>
+                <div className="logo">{props.title}</div>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li className="dropdown"><a href="">Category</a>
+                            <ul>
+                                <li><a href="">Men</a></li>
+                                <li><a href="">Women</a></li>
+                                <li><a href="">Kid</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+Header.propTypes = {
+    title: propTypes.string
+}
+
+Header.defaultProps = {
+    title: "logo"
 }
