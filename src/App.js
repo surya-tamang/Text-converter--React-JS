75717773aaa0714ae725f38af9f12da96edb29cb
evil-89789 <tngsurya055@gmail.com>
2024-03-31 18:09:09 +0545

    props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
+import Header from './components/Navbar';
 
 function App() {
   return (
     <>
-      <header>
-        <div className="logo">Fashion<span>Bazar</span></div>
-        <nav>
-          <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li className="dropdown"><a href="">Category</a>
-              <ul>
-                <li><a href="">Men</a></li>
-                <li><a href="">Women</a></li>
-                <li><a href="">Kid</a></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header title="Company name"/>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Header(props){
+return (
+    <div>
+        <header>
+        <div className="logo">{props.title}</div>
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li className="dropdown"><a href="">Category</a>
+              <ul>
+                <li><a href="">Men</a></li>
+                <li><a href="">Women</a></li>
+                <li><a href="">Kid</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+)
+}
