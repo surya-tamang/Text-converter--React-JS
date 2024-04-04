8ba4470c1062f49f56deaacf2de56390a9d998b6
evil-89789 <tngsurya055@gmail.com>
2024-04-04 07:02:37 +0545

    textarea added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Main from './components/Main';
 import Header from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Header title ="FashionBazar"/>
+
+      <div className='container-fluid d-flex justify-content-center min-vh-100 bg-dark-subtle align-items-center'>
+        
+        <Header title="FashionBazar" />
+        <Main />
+        
+      </div>
+
     </>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main(){
+    return (
+        <main className="w-100 min-vh-100 d-flex  flex-column justify-content-center align-items-center bg-primary">
+            <textarea className="w-50 mh-50"></textarea>
+            <h3 className="mt-3">Convert the text into</h3>
+        </main>
+    )
+}
