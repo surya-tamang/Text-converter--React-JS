b1158d2a74254448767f56657a9faac80282240d
evil-89789 <tngsurya055@gmail.com>
2024-04-05 19:03:15 +0545

    main page modified

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-export default function Main(){
+export default function Main() {
     return (
-        <main className="w-100 min-vh-100 d-flex  flex-column justify-content-center align-items-center bg-primary">
-            <textarea className="w-50 mh-50"></textarea>
+        <main className="w-100 min-vh-100 d-flex  flex-column justify-content-center align-items-center mt-4">
+            <div className="container">
+                <textarea className="w-50 mh-50" id="input" placeholder="Enter the text here"></textarea>
+                <textarea className="w-50 mh-50" id="output" disabled></textarea>
+            </div>
             <h3 className="mt-3">Convert the text into</h3>
+            <br />
+            <div className="container d-flex justify-content-center">
+                <button>Uppercase</button><button>Lowercase</button>
+            </div>
         </main>
     )
 }
